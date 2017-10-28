dc7df1d3577c2abff705a53f242a79086c9309bf
yedim <yelimi0416@gmail.com>
2017-10-28 10:44:33 +0900

    SetBusRoad

--- BusAlarm/src/BusAlarmScreen/BusRoad.java
+++ BusAlarm/src/BusAlarmScreen/BusRoad.java
@@ -3,18 +3,25 @@ package BusAlarmScreen;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class BusRoad extends JPanel {
 
-	//Image icbusIcon=new ImageIcon(this.getClass().getResource("/BusIcon.png")).getImage();
+	Image icbusIcon=new ImageIcon(this.getClass().getResource("/BusIcon.png")).getImage();
 
-	
+	Point pos; //버스 좌표
+	JButton bbusRoad;
+
+	BusRoad(int x, int y) {
+		pos = new Point(x, y); //버스의 좌표를 체크
+	}
 
 }
